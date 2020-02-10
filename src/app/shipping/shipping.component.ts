00fe31b129e6270520c431b91c4896c08c6c4962
irazirfan <iraz@mislbd.com>
2020-02-11 00:29:31 +0600

    fix spelling issue

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CartService } from '../cart.Service';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-shipping',
